4ac62dfdf4b5dbca81709a3d7593470ab26b59be
Tiago Souza <tiagoadesouza21@gmail.com>
2025-07-18 14:39:30 -0300

    recreating structure docker compose

--- Events-Microservice/src/main/java/com/microservice/events/Events/Microservice/domain/service/EmailService.java
+++ Events-Microservice/src/main/java/com/microservice/events/Events/Microservice/domain/service/EmailService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "email-service", url = "http://localhost:8081/api/email")
+@FeignClient(name = "email-service", url = "http://mail:8081/api/email")
 public interface EmailService {
 
     @PostMapping("/send")
